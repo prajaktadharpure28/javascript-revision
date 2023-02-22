a286e5bfda6ab8e26a9f598121ea576ef7237602
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-22 20:22:11 +0530

    variables

---
+++ 01_variables/var.js
@@ -0,0 +1,18 @@
+// var is globally scoped 
+// var can be updated & re-declared within its scope
+// var is function scoped
+
+var fullName = "Prajakta Dharpure";
+var fullName = "Prajakta Sanjay Dharpure";
+var courseName = "JS Bootcamp";
+
+console.log(fullName);
+console.log(courseName);
+
+var courseName = "JavaScript Bootcamp";
+console.log(courseName);
+
+var age =22;
+console.log(age);
+
+
