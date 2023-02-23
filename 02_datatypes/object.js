9afa850a003a36a2362679e69219dd260eeb8466
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 16:57:53 +0530

    objects

---
+++ 02_datatypes/object.js
@@ -0,0 +1,11 @@
+// Non Primitive Data Type - Objects in Js
+
+const item ={
+    "Praju": true,
+    "age": 22,
+    "phoneNo": 1234567890,
+    "rollNo": undefined
+}
+
+console.log(item["age"]);
+console.log(item.Praju);
