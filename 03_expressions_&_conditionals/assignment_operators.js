101760246ac4c956a957f626481383be9ab2abe5
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 17:34:38 +0530

    assignment operators

---
+++ 03_expressions_&_conditionals/assignment_operators.js
@@ -0,0 +1,29 @@
+// Assignment Operators
+
+let a = 10;
+let b = 4;
+
+// a = a + b;
+a += b;
+console.log("a += b = ", a);
+
+// a = a - b;
+a -= b;
+console.log("a -= b = ", a);
+
+// a = a * b;
+a *= b;
+console.log("a *= b = ", a);
+
+// a = a / b;
+a /= b;
+console.log("a /= b = ", a);
+
+// a = a % b;
+a %= b;
+console.log("a %= b = ", a);
+
+// a = a ** b;
+a **= b;
+console.log("a **= b = ", a);
+
