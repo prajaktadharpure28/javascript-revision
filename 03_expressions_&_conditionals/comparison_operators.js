43c4661173d9adea104833f9687326d869bc421f
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 17:58:36 +0530

    comparison operator: logical or

--- 03_expressions_&_conditionals/comparison_operators.js
+++ 03_expressions_&_conditionals/comparison_operators.js
@@ -52,3 +52,10 @@ console.log("true && false : ", true && false);
 console.log("false && true : ", false && true);
 console.log("false && false : ", false && false);
 
+// Logical OR
+
+console.log("true || true : ", true || true);
+console.log("true || false : ", true || false);
+console.log("false || true : ", false || true);
+console.log("false || false : ", false || false);
+
