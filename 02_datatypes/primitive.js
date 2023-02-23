42059f30c02ae711f3294184174121c7fbfbba45
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 16:52:00 +0530

    primitive datatypes

---
+++ 02_datatypes/primitive.js
@@ -0,0 +1,22 @@
+// Primitive Datatypes
+
+// nn bb ss u
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+// 7. BigInt
+
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3");
+let e = "Praju";
+let f = Symbol("I am a symbol");
+let g = undefined;
+
+console.log(a, b, c, d, e, f, g)
+console.log(typeof d)
