7c44e16af335fc0dd9fc7a852965a5e08d57beda
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 18:28:15 +0530

    Conditional Expression: if else statement

--- 04_conditional_expression/conditional.js
+++ 04_conditional_expression/conditional.js
@@ -9,3 +9,13 @@ if (a > b) {
     console.log("a is greater than b");
     }
 
+// If Else Statement
+
+if (a < b) {
+    console.log("a is less than b");
+    }
+else {
+    console.log("a is greater than b");
+    }
+
+    
