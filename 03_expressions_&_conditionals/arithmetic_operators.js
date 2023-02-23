2091e5314bf7dc0b4d163d56469f60a396dadd17
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-23 17:27:33 +0530

    arithmetic operators

--- 03_expressions_&_conditionals/arithmetic_operators.js
+++ 03_expressions_&_conditionals/arithmetic_operators.js
@@ -3,12 +3,12 @@
 let a = 10;
 let b = 4;
 
-// console.log("a + b = ", a + b);
-// console.log("a - b = ", a - b);
-// console.log("a / b = ", a / b);
-// console.log("a * b = ", a * b);
-// console.log("a ** b = ", a ** b);
-// console.log("a % b = ", a % b);
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a / b = ", a / b);
+console.log("a * b = ", a * b);
+console.log("a ** b = ", a ** b);
+console.log("a % b = ", a % b);
 
 console.log("++a = ", ++a); 
 console.log("a++ = ", a++); 
