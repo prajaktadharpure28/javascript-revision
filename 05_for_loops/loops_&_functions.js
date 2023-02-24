7bf2614045884dd0dafdec79ac61296501bed00f
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-24 23:05:41 +0530

    for loop

---
+++ 05_for_loops/loops_&_functions.js
@@ -0,0 +1,16 @@
+// Types of for loops
+// 1. for loop
+// 2. for in loop
+// 3. for of loop
+// 4. for each loop
+
+// for loop
+
+// for (initialization; condition; increment/decrement) {   
+//     // code block to be executed
+// }
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
