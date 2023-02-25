9b1c290770efbb65b54af11a886641da83312274
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-25 22:11:01 +0530

    for loop examples

--- 05_for_loops/for_loop.js
+++ 05_for_loops/for_loop.js
@@ -43,3 +43,15 @@ for (let i = 0; i < 10; i++) {
     }
     console.log(i);
 }
+
+// Example 7
+//Program to add first n natural numbers
+
+let n = 10;
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum);
+
+
