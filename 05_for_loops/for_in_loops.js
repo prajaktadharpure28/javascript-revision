9474b8c6b75da9f202d42219c51bd9d4e036d228
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-25 22:32:53 +0530

    for in loop

---
+++ 05_for_loops/for_in_loops.js
@@ -0,0 +1,25 @@
+// for in loops
+
+// for in loops are used to iterate over the properties of an object
+
+// for in loops are not used to iterate over arrays
+
+// for in loops are used to iterate over objects
+
+// Example 1
+
+// wap to print name, age and city using for in loop.
+
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+for (let key in person) {
+    console.log(key, person[key]);
+}
+
+
+
+
