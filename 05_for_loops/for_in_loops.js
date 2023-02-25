28c2fa3fb702d2a2d80cf420e5bd015a2dfa89be
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-25 22:38:07 +0530

    print the values of an object using for in loop

--- 05_for_loops/for_in_loops.js
+++ 05_for_loops/for_in_loops.js
@@ -34,3 +34,17 @@ for (let key in person1) {
     console.log(key);
 }
 
+// Example 3
+
+// wap to print the values of an object using for in loop.
+
+let person2 = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+for (let key in person2) {
+    console.log(person2[key]);
+}
+
