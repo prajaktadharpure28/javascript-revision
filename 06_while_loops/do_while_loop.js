36dc619ca09574da3ff58954997969cb877425d4
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-28 20:47:55 +0530

    print the numbers from 1 to 10 using do while loop

---
+++ 06_while_loops/do_while_loop.js
@@ -0,0 +1,20 @@
+// Do while loop
+
+// do while loop is used to execute a block of code repeatedly until a condition is false.
+
+// do while loop is similar to while loop except that the condition is checked at the end of the loop.
+
+// do while loop is used to execute a block of code at least once.
+
+// Example 1
+
+// wap to print the numbers from 1 to 10 using do while loop.
+
+let i = 1;
+
+do {
+    console.log(i);
+    i++;
+}
+while (i <= 10);
+
