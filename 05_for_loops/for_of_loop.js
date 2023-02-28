8b8b478f5965d59e186e28f7419173f32e84ec30
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-02-28 20:42:29 +0530

    print the numbers from 1 to 10 using while loop

--- 05_for_loops/for_of_loop.js
+++ 05_for_loops/for_of_loop.js
@@ -15,4 +15,3 @@ let arr = [1, 2, 3, 4, 5];
 for (let element of arr) {
     console.log(element);
 }
-
---
+++ 06_while_loops/while_loop.js
@@ -0,0 +1,16 @@
+// While loop
+
+// while loop is used to execute a block of code repeatedly until a condition is false.
+
+
+// Example 1
+
+// wap to print the numbers from 1 to 10 using while loop.
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
