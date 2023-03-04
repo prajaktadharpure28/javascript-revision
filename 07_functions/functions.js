66e4d1d97aa15eb22da81a6af5faf0c6e159928a
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-04 22:52:35 +0530

    calculate average using function

---
+++ 07_functions/functions.js
@@ -0,0 +1,20 @@
+// Functions
+
+// A function is a block of code designed to perform a particular task.
+
+// A function is executed when "something" invokes it (calls it).
+
+// Example 1
+
+function onePlusAvg(x,y){
+    return 1 + (x + y) / 2
+}
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+console.log(onePlusAvg(a,b));
+console.log(onePlusAvg(a,c));
+console.log(onePlusAvg(b,c));
+
