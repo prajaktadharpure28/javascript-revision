830a58a51f928ad92000cba2229ab5432d8f3811
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-09 22:22:45 +0530

    docs: print marks of students in an object using for of loop

--- 05_for_loops/ps.js
+++ 05_for_loops/ps.js
@@ -18,3 +18,10 @@ for(let i=0;i<Object.keys(marks).length;i++){
 for(let i in marks){
     console.log("Marks of "+i+" are "+marks[i])
 }
+
+// 3. Write a program to print the marks of a student in an object using for of loop
+
+for(let i of Object.keys(marks)){
+    console.log("Marks of "+i+" are "+marks[i])
+}
+
