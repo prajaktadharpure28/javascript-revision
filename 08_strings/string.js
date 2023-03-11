29491aa28586e279caaeaed7a9c080f15b20d61f
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-11 22:37:19 +0530

    string

---
+++ 08_strings/string.js
@@ -0,0 +1,15 @@
+// String
+
+// String are used to store and manipulate text. 
+// A string is a series of characters like "John Doe".
+
+// String are created with double or single quotes:
+var carName1 = "Volvo XC60"; // Using double quotes
+var carName2 = 'Volvo XC60'; // Using single quotes
+
+
+
+
+
+
+
