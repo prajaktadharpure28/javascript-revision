4c61d41aa11073a66b477645d79b0ed2b6a30604
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-12 12:44:35 +0530

    String Method: upper case

--- 08_strings/string_methods.js
+++ 08_strings/string_methods.js
@@ -7,4 +7,9 @@
 let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 console.log(txt.length);
 
+// toUpperCase
 
+// The toUpperCase() method converts a string to uppercase letters:
+
+let txt1 = "Hello World!";
+console.log(txt1.toUpperCase());
