f7d7321d7ecdd63c9516a441105cebdffc28f779
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-13 00:00:33 +0530

    String Method: slice

--- 08_strings/string_methods.js
+++ 08_strings/string_methods.js
@@ -21,4 +21,18 @@ console.log(txt1.toUpperCase());
 let txt2 = "Hello World!";
 console.log(txt2.toLowerCase());
 
+// slice
 
+// The slice() method extracts a part of a string and returns the extracted part in a new string.
+
+// The method takes 2 parameters: the start position, and the end position (end not included).
+
+// This example slices out a portion of a string from position 7 to position 13:
+
+// Example
+
+// Extract parts of a string:
+
+let str = "Apple, Banana, Kiwi";
+let res = str.slice(7, 13);
+console.log(res);
