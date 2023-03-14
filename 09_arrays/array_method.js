61babb71f97eb3a3017ca938e0a838c985ec49e6
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-14 16:48:22 +0530

    Array Method: pop()

--- 09_arrays/array_method.js
+++ 09_arrays/array_method.js
@@ -31,3 +31,15 @@ let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(num1.join(" - "));
 console.log(num1.join(""));
 
+// 3. pop() - Removes the last element from an array.
+
+// Example
+
+let fruits2 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits2.pop());
+
+// Example
+
+let num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(num2.pop());
+
