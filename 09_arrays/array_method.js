d6961d971a1dc466cc0cce9034d4551004ed4866
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-14 16:39:57 +0530

    Array Method: join()

--- 09_arrays/array_method.js
+++ 09_arrays/array_method.js
@@ -15,3 +15,19 @@ console.log(fruits.toString());
 
 let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(num.toString());
+
+// 2. join() - Joins all array elements into a string.
+
+// It behaves just like toString(), but in addition you can specify the separator.
+
+// Example
+
+let fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits1.join(" * "));
+
+// Example
+
+let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(num1.join(" - "));
+console.log(num1.join(""));
+
