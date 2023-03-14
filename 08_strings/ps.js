b2c77115e96bc6a4c4574a2812069f58b7c4dff7
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-14 11:27:01 +0530

    string: practice set

---
+++ 08_strings/ps.js
@@ -0,0 +1,10 @@
+// What will the following print in javascript?
+console.log("har\"".length)
+
+let str = "har\""
+console.log(str.length)
+
+// 4
+
+// Explore the includes, starts with & ends with function of a string.
+
