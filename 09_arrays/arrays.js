05c224f7ac674a1d4aa0f43de55e508dfde95ac1
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-14 11:31:25 +0530

    array of strings

---
+++ 09_arrays/arrays.js
@@ -0,0 +1,13 @@
+//  Arrays
+
+// Arrays are variables which can hold more than one values.
+// Arrays are created using square brackets.
+// Arrays are zero-indexed, meaning the first element is at index 0.
+// Arrays can be accessed using bracket notation.
+
+// Example 1
+// Create an array of strings
+
+let names = ['John', 'Mark', 'Jane'];
+console.log(names);
+
