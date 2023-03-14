8adea59a1b68a7f998c77bf174c9c1768b5e470d
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2023-03-14 16:35:04 +0530

    Array Method: toString()

---
+++ 09_arrays/array_method.js
@@ -0,0 +1,17 @@
+// Array Methods
+
+// Array Methods are functions that are built into JavaScript and can be used on arrays.
+
+// The most common array methods are:
+
+// 1. toString() - Converts an array to a string of (comma separated) array values.
+
+// Example
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.toString());
+
+// Example
+
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(num.toString());
